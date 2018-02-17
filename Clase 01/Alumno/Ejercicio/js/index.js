5427f4b5d8b33653ab16ee2aea99b868686152ba
gonzalopetraglia <gonzalopetraglia@gmail.com>
2018-02-17 13:49:01 -0300

    clase 1

---
+++ Clase 01/Alumno/Ejercicio/js/index.js
@@ -0,0 +1,20 @@
+var name = prompt('Ingrese su nombre', 'Gon')
+console.log('Hola '+ name + '!')
+
+var number1Suma = prompt('Ingrese un numero')
+var parcednumber1Suma = parseInt(number1Suma,10)
+var number2Suma= 5
+console.log('la suma es: ',parcednumber1Suma+number2Suma)
+
+
+var number1Resta = prompt('Ingrese un numero')
+var parcednumber1Resta = parseInt(number1Resta,10)
+var number2Resta= 5
+console.log('la resta es: ',number2Resta-parcednumber1Resta)
+
+
+var number1Suma2 = prompt('Ingrese un numero')
+var parcednumber1Suma2 = parseInt(number1Suma2,10)
+var number2Suma2 = prompt('Ingrese otro numero')
+var parcednumber2Suma2 = parseInt(number2Suma2,10)
+console.log('la suma de los dos numeros es: ',parcednumber1Suma2+parcednumber2Suma2)
